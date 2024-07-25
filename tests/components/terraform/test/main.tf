9ad18af6056b9c403795f8f84acefc6513772a50
Dan Miller <miller0daniel@gmail.com>
2024-07-25 13:05:00 -0400

    Fix Release (#30)
    * resolve tflint complaints
    
    * removed tf module workflows
    
    * corrected reusable workflows

--- tests/components/terraform/test/main.tf
+++ tests/components/terraform/test/main.tf
@@ -6,7 +6,7 @@ variable "seed" {
 resource "random_id" "this" {
   keepers = {
     # Generate a new id each time we switch to a new seed
-    seed = "${var.seed}"
+    seed = var.seed
   }
   byte_length = 8
 }
